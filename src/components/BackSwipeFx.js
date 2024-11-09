d59f99f2827f8027380675b7a78d300a55f5f254
aksh0000 <75081649+aksh0000@users.noreply.github.com>
2024-11-09 11:04:48 -0800

    Delete src directory

--- src/components/BackSwipeFx.js
+++
@@ -1,7 +0,0 @@
-const back_screen=(index)=>{
-    const first_slide=document.getElementById("register_form");
-  
-    first_slide.style.transform=`translateX(-${index*100}vw)`;
-}
-
-export default back_screen;
--- src/components/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Dashboard.css'
-export default function Dashboard() {
-  return (
-    <div id="dashboard_container">
-        <p style={{color:'white'}}>Dashboard</p>
-    </div>
-  )
-}
