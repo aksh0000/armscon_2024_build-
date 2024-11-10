bd353fea5e62e07f50e7d921951e549dc69688af
aksh0000 <75081649+aksh0000@users.noreply.github.com>
2024-11-10 15:41:21 -0800

    Delete src/components directory

--- src/components/Swipe_functions.js
+++
@@ -1,8 +0,0 @@
-const swipe=(index)=>{
-    const first_slide=document.getElementById("register_form");
-
-    first_slide.style.transform=`translateX(-${index*100}vw)`;
-
-}
-
-export default swipe;
